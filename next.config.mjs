f7522ba66d1a64d29b53cbf4b49bcb9c58e37cb4
Umer-Aziz <umars25271997@gmail.com>
2024-02-25 21:30:42 +0500

    video downloader

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
           {
-            hostname: 'img.youtube.com',  
+            hostname: '*',  
           },
         ],
       },
